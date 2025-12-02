150c8c909bfd1037d95fde9c7d448f73f8043370
FantasticOnRye <melody.adamson2015@gmail.com>
2025-12-01 22:33:29 -0500

    added form to insights page

--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useScrollSlideUp from '../hooks/useScrollSlideUp';
+import Form from '../components/form';
 
 const Insights = () => {
   const [ref1, inView1] = useScrollSlideUp();
@@ -11,6 +12,28 @@ const Insights = () => {
       <h1 className="text-[#c8102e] mb-4 font-extrabold" style={{ fontSize: '100px' }}>
         RPI Says
       </h1>
+
+      <div
+        ref={ref1}
+        className={`transition-all duration-700 ${
+          inView1 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'
+        }`}
+      >
+      </div>
+
+      <div
+        ref={ref2}
+        className={`transition-all duration-700 ${
+          inView2 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'
+        }`}
+      ></div>
+
+      <div
+        ref={ref3}
+        className={`transition-all duration-700 ${
+          inView3 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'
+        }`}
+      ></div>
 
     </div>
   );
